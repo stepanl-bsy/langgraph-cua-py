a368d003a91445797662174c2c06c26a9ac71d25
bracesproul <braceasproul@gmail.com>
2025-03-24 16:40:27 -0700

    better

--- examples/price-finder.py
+++ examples/price-finder.py
@@ -1,6 +1,10 @@
+from typing import List, Literal
+
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
 from langgraph.graph import END, START, StateGraph
+from pydantic import BaseModel, Field
 
 from langgraph_cua import create_cua
 from langgraph_cua.types import CUAState
@@ -14,23 +18,62 @@ cua_graph = create_cua()
 
 
 class PriceFinderState(CUAState):
-    pass
+    route: Literal["respond", "computer_use_agent"]
 
 
 def process_input(state: PriceFinderState):
-    # Take user's input. if they want to get the price of something, route to computer use
-    # else, route to respond
-    pass
+    system_message = SystemMessage(
+        content=(
+            "You're an advanced AI assistant tasked with routing the user's query to the appropriate node."
+            + "Your options are: computer use or respond. You should pick computer use if the user's request requires "
+            + "using a computer (e.g. looking up a price on a website), and pick respond for ANY other inputs."
+        )
+    )
+
+    class RoutingToolSchema(BaseModel):
+        """Route the user's request to the appropriate node."""
+
+        route: Literal["respond", "computer_use_agent"] = Field(
+            ...,
+            description="The node to route to, either 'computer_use_agent' for any input which might require using a computer to assist the user, or 'respond' for any other input",
+        )
+
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    model_with_tools = model.with_structured_output(RoutingToolSchema)
+
+    messages = [system_message, HumanMessage(content=state.messages[-1].content)]
+
+    response = model_with_tools.invoke(messages)
+    return {"route": response.route}
 
 
 def respond(state: PriceFinderState):
-    # pass in full message history, reply to user
-    pass
+    def format_messages(messages: List[AnyMessage]) -> str:
+        return "\n".join([f"{message.type}: {message.content}" for message in messages])
+
+    system_message = SystemMessage(
+        content=(
+            "You're an advanced AI assistant tasked with responding to the user's input."
+            + "You're provided with the full conversation between the user, and the AI assistant. "
+            + "This conversation may include messages from a computer use agent, along with "
+            + "general user inputs and AI responses. \n\n"
+            + "Given all of this, please RESPOND to the user. If there is nothing to respond to, you may return something like 'Let me know if you have any other questions.'"
+        )
+    )
+    human_message = HumanMessage(
+        content="Here are all of the messages in the conversation:\n\n"
+        + format_messages(state.messages)
+    )
+
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+    response = model.invoke([system_message, human_message])
+    return {"response": response}
 
 
 def route_after_processing_input(state: PriceFinderState):
     # Route to computer use or respond based on input
-    pass
+    return state.get("route")
 
 
 workflow = StateGraph(PriceFinderState)
