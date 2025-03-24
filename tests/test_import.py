f14cad41feb533185e70f293fdee31ebaf7b9d81
bracesproul <braceasproul@gmail.com>
2025-03-24 12:21:57 -0700

    fix tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,3 @@
 def test_import() -> None:
     """Test that the code can be imported"""
-    from langgraph_cua import (  # noqa: F401
-        add_active_agent_router,
-        create_handoff_tool,
-        create_swarm,
-    )
+    from langgraph_cua import CUAState, create_cua, graph  # noqa: F401
